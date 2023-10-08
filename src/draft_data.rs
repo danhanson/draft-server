918b7369ff770c3d34a80dd5d597330fc2eb017f
danhanson <danielghanson93@gmail.com>
2023-10-08 02:14:07 -0500

    rewrote code

--- src/draft_data.rs
+++ src/draft_data.rs
@@ -1,39 +1,35 @@
 use std::collections::HashMap;
-use std::sync::RwLock;
 use axum::extract::ws;
 
 #[derive(Debug)]
 pub struct DraftData {
-    teams: RwLock<HashMap<String, String>>,
+    teams: HashMap<String, String>,
 }
 
 impl DraftData {
     pub fn new() -> Self {
         DraftData {
-            teams: RwLock::new(HashMap::new()),
+            teams: HashMap::new(),
         }
     }
-    pub fn update(&self, id: String, team: String) -> ws::Message {
-        let mut teams = self.teams.write().unwrap();
+    pub fn update(&mut self, id: String, team: String) -> ws::Message {
         let msg = ws::Message::Text(format!("{{\"action\":\"update\",\"id\":\"{}\",\"value\":{}}}", id, team));
-        teams.insert(id, team);
+        self.teams.insert(id, team);
         msg
     }
-    pub fn delete(&self, id: &str) -> Option<ws::Message> {
-        let mut teams = self.teams.write().unwrap();
-        teams.remove(id).map(
+    pub fn delete(&mut self, id: &str) -> Option<ws::Message> {
+        self.teams.remove(id).map(
             |_| ws::Message::Text(format!("{{\"action\":\"delete\",\"id\":\"{}\"}}", id))
         )
     }
     pub fn load(&self) -> String {
-        let teams = self.teams.read().unwrap();
         let front = "{{\"action\":\"load\",\"value\":[";
         let back = "]}";
-        let team_length = teams.values().into_iter().map(|item| item.len() + 1).reduce(|x, y| x + y).unwrap_or(1) - 1;
+        let team_length = self.teams.values().into_iter().map(|item| item.len() + 1).reduce(|x, y| x + y).unwrap_or(1) - 1;
         let length = front.len() + back.len() + team_length;
         let mut message = String::with_capacity(length);
         message.push_str(front);
-        let mut iter = teams.values().into_iter();
+        let mut iter = self.teams.values().into_iter();
         if let Some(first) = iter.next() {
             message.push_str(first);
             for team in iter {
@@ -42,6 +38,6 @@ impl DraftData {
             }
         }
         message.push_str(back);
-        return message;
+        message
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use axum::{
     routing::{get, put},
     Router, http::StatusCode,
 };
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 use std::{net::SocketAddr, str::FromStr};
 use tower_http::services::ServeDir;
 use tokio::sync::broadcast;
@@ -22,21 +22,19 @@ const ASSETS_DIR: &str = "assets";
 #[derive(Debug, Clone)]
 struct AppState {
     websocket_channel: broadcast::Sender<ws::Message>,
-    draft_data: Arc<DraftData>,
+    draft_data: Arc<RwLock<DraftData>>,
 }
-
 
 impl AppState {
     fn new(websocket_channel: broadcast::Sender<ws::Message>) -> Self {
         Self {
             websocket_channel: websocket_channel,
-            draft_data: Arc::new(DraftData::new()),
+            draft_data: Arc::new(RwLock::new(DraftData::new())),
         }
     }
 }
 
-
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() {
     let args: Vec<String> = std::env::args().collect();
 
@@ -53,7 +51,6 @@ async fn main() {
     let addr = SocketAddr::from_str(&args[1]).expect("bad argument");
     axum::Server::bind(&addr).serve(routes.into_make_service_with_connect_info::<SocketAddr>()).await.expect("failed to start server");
 }
-
 
 async fn log_ws_messages(mut receiver: broadcast::Receiver<ws::Message>) {
     loop {
@@ -76,71 +73,81 @@ async fn websocket_route(State(state): State<AppState>, ws: WebSocketUpgrade) ->
 }
 
 async fn subscribe(state: AppState, mut socket: ws::WebSocket) {
-    let mut receiver = state.websocket_channel.subscribe();
-    let data = state.draft_data.load();
-    if let Err(error) = socket.send(ws::Message::Text(data)).await {
+    let (result, mut receiver) = {  // use block scope to avoid Send error, see https://github.com/rust-lang/rust/issues/57478
+        let data = state.draft_data.read().unwrap();
+        let result = ws::Message::Text(data.load());
+        let receiver = state.websocket_channel.subscribe();
+        drop(data);  // hold lock after subscribe to avoid missing writes
+        (result, receiver)
+    };
+    if let Err(error) = socket.send(result).await {
         print!("Unexpected error: {:?}", error);
-        let _ = socket.close();
+        if let Err(error) = socket.close().await {
+            print!("Unexpected error: {:?}", error);
+        }
         return;
     }
     loop {
-        tokio::select! {
+        let result = tokio::select! {
+            biased;  // prioritize processing the channel to avoid Lagged error
+            update_message = receiver.recv() => {
+                match update_message {
+                    Ok(msg) => Some(msg),
+                    Err(broadcast::error::RecvError::Lagged(_)) => {
+                        println!("LAGGING!");
+                        None
+                    },
+                    Err(broadcast::error::RecvError::Closed) => None
+                }
+            },
             ws_message = socket.recv() => {
                 match ws_message {
                     None => {
                         break;
                     },
-                    Some(Ok(ws::Message::Close(_))) => {
-                        let _ = socket.close().await;
-                        break;
-                    },
+                    Some(Ok(ws::Message::Close(_))) => None,
                     Some(Ok(msg)) => {
                         print!("Unexpected message: {:?}", msg);
+                        continue;
                     },
                     Some(Err(error)) => {
                         print!("Unexpected error: {:?}", error);
-                        let _ = socket.close().await;
-                        break;
+                        None
                     }
                 }
-                todo!()
             },
-            update_message = receiver.recv() => {
-                match update_message {
-                    Ok(msg) => {
-                        if let Err(error) = socket.send(msg).await {
-                            println!("Unexpected error: {:?}", error);
-                        }
-                    },
-                    Err(broadcast::error::RecvError::Lagged(_)) => {
-                        println!("LAGGING!");
-                    },
-                    Err(broadcast::error::RecvError::Closed) => {
-                        break;
-                    }
-                }
+        };
+        // extract async code from select! to ensure that its tasks are cancellation safe
+        if let Some(msg) = result {
+            if let Err(error) = socket.send(msg).await {
+                println!("Unexpected error: {:?}", error);
             }
+        } else {
+            if let Err(error) = socket.close().await {
+                println!("Unexpected error: {:?}", error)
+            }
+            break;
         }
     }
 }
 
-
 async fn put_team(State(state): State<AppState>, Path(team_id): Path<String>, body: String) -> impl IntoResponse {
-    let result = state.draft_data.update(team_id, body);
-    if let Err(error) = state.websocket_channel.send(result) {
-        println!("Unexpected error: {:?}", error);
-    }
+    let mut draft_data = state.draft_data.write().unwrap();
+    let msg = draft_data.update(team_id, body);
+    let _ = state.websocket_channel.send(msg);
+    drop(draft_data);  // hold lock to after send to ensure writes in correct order
     StatusCode::OK
 }
 
 async fn delete_team(State(state): State<AppState>, Path(team_id): Path<String>) -> impl IntoResponse {
-    match state.draft_data.delete(&team_id) {
-        Some(msg) => {
-            if let Err(error) = state.websocket_channel.send(msg) {
-                println!("Unexpected error: {:?}", error);
-            }
-            StatusCode::OK
-        },
-        None => StatusCode::NOT_FOUND
+    let mut draft_data = state.draft_data.write().unwrap();
+    let is_deleted = draft_data.delete(&team_id).map(
+        |msg| state.websocket_channel.send(msg)
+    ).is_some();
+    drop(draft_data);  // hold lock to after send to ensure writes in correct order
+    if is_deleted {
+        StatusCode::OK
+    } else {
+        StatusCode::NOT_FOUND
     }
 }
